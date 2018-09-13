7c8918e871afd13241d7640b323272d004e8404b
Warid13 <43229214+Warid13@users.noreply.github.com>
2018-09-13 11:32:09 +0600

    Add files via upload

---
+++ 2.MvcIntroWebApp/MvcIntroWebApp/Gateway/DepartmentGateway.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using MvcIntroWebApp.Models;
+using MvcIntroWebApp.Models.ViewModel;
+
+namespace MvcIntroWebApp.Gateway
+{
+    public class DepartmentGateway
+    {
+        public SqlConnection Connection { get; set; }
+        public SqlDataReader Reader { get; set; }
+        public SqlCommand Command { get; set; }
+        public DepartmentGateway()
+        {
+            string connectionString =
+                WebConfigurationManager.ConnectionStrings["UniversityDBConString"].ConnectionString;
+
+            Connection = new SqlConnection(connectionString);
+        }
+
+
+        public List<Department> GetAllDepartment()
+        {
+            string query = "SELECT * FROM Department";
+
+            Command = new SqlCommand(query, Connection);           
+            Connection.Open();
+            Reader = Command.ExecuteReader();
+
+            List<Department> departments = new List<Department>();
+            while (Reader.Read())
+            {
+                Department department = new Department();
+                department.Id = Convert.ToInt32(Reader["Id"]);
+                department.Name = Reader["Name"].ToString();
+                departments.Add(department);
+            }
+
+            Reader.Close();
+            Connection.Close();
+            return departments;
+        }
+    }
+}
